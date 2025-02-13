a2878f25fc0e92c2952fa1482b526b126a2c6cdb
Nayesh Daggula <49321720+nayeshdaggula@users.noreply.github.com>
2025-02-13 12:01:34 +0530

    newupdates

--- src/components/Group/index.tsx
+++ src/components/Group/index.tsx
@@ -1,35 +1,66 @@
-import React from 'react';
-import clsx from 'clsx';
+import React from "react";
+import clsx from "clsx";
 
 interface GroupProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
-  gap?: string; // Tailwind gap utility (e.g., 'gap-4', 'gap-2')
+  gap?: "xl" | "lg" | "md" | "sm" | "xs"; // Gap between elements
   grow?: boolean; // If true, all items should grow equally in one line
-  justify?: 'flex-start' | 'center' | 'flex-end' | 'space-between'; // Added justification options
+  justify?: "flex-start" | "center" | "flex-end" | "space-between";
 }
 
 const Group: React.FC<GroupProps> = ({
   children,
-  gap = 'gap-4',
+  gap = "md",
   grow = false,
-  justify = 'flex-start', // Default justify to 'flex-start'
+  justify = "flex-start",
   className,
   ...props
 }) => {
-  // If grow is true, all items should be on one line and grow equally
+
+  const getJustifyclass = (justify: string) => {
+    switch (justify) {
+      case "flex-start":
+        return "justify-start";
+      case "center":
+        return "justify-center";
+      case "flex-end":
+        return "justify-end";
+      case "space-between":
+        return "justify-between";
+      default:
+        return "justify-start";
+    }
+  }
+
+  const gapClasses = (gap: string) => {
+    switch (gap) {
+      case "xl":
+        return "gap-8";
+      case "lg":
+        return "gap-6";
+      case "md":
+        return "gap-4";
+      case "sm":
+        return "gap-2";
+      case "xs":
+        return "gap-1";
+      default:
+        return "gap-4";
+    }
+  }
+
   const groupClasses = clsx(
-    'flex', // Set display to flex
-    gap, // Apply gap between elements
-    grow ? 'justify-between flex-nowrap' : justify, // If grow is true, use 'justify-between'; otherwise, apply the provided justify value
-    grow ? '' : 'flex-wrap', // If grow is false, enable wrapping
+    "flex", // Set display to flex
+    gapClasses(gap), // Set gap between
+    grow ? "flex-nowrap justify-between" : `${getJustifyclass(justify)}`, 
     className // Allows for additional custom classes
   );
 
   return (
     <div className={groupClasses} {...props}>
       {React.Children.map(children, (child) =>
-        grow ? (
-          <div className="flex-grow">{child}</div> // Each child grows equally if grow is true
+        grow && React.isValidElement(child) ? (
+          <div className="flex-grow">{child}</div>
         ) : (
           child
         )
